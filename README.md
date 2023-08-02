b53f5d72bca47ea94c0feffb68d149137b1988f7
matiasrojo <matiasrojo011@gmail.com>
2023-08-02 19:46:58 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Frontend Mentor - Base Apparel coming soon page solution
+
+This is a solution to the [Base Apparel coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+### Links
+
+- Live Site URL: 
+
+### Screenshot
+
+![](./design/desktop-design.jpg)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+### Built with
+
+- HTML
+- CSS
+- Javascript
+
+## Author
+
+- Frontend Mentor - [@matiasroj0](https://www.frontendmentor.io/profile/matiasroj0)
+- LinkedIn - https://www.linkedin.com/in/matias-rojo-49aa2a197/
---
+++ index.js
@@ -0,0 +1,42 @@
+const emailInput = document.querySelector(".email-input");
+const submitBtn = document.querySelector(".email-submit-btn");
+const emailError = document.querySelector(".email-error");
+const errorIcon = document.querySelector(".error-icon");
+const card = document.querySelector(".card");
+const success = document.querySelector(".success");
+const successEmail = document.querySelector(".success-email");
+
+submitBtn.addEventListener("click", ()=> {
+    if(!ValidateEmail()) {
+        emailError.classList.remove("hidden");
+        emailInput.classList.add("email-input-error");
+        errorIcon.classList.remove("hidden");
+        success.classList.add("hidden");
+    }
+    else {
+        emailError.classList.add("hidden");
+        emailInput.classList.remove("email-error");
+        emailInput.classList.remove("email-input-error");
+        errorIcon.classList.add("hidden");
+        success.classList.remove("hidden");
+    }
+})
+
+// dismissBtn.addEventListener("click", ()=> {
+//     card.classList.remove("hidden");
+//     success.classList.add("hidden");
+// })
+
+
+function ValidateEmail(){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)){
+        emailInput.focus();
+        return true;
+    }
+    else {
+        emailInput.focus();
+        return false;
+    }
+}
+
+
